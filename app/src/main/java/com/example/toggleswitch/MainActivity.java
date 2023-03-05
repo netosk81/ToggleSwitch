7c38e9e36be120ed0f8d42a95651b74f1b0e21c2
netosk81 <hk.neto@gmail.com>
2023-03-05 09:35:47 -0300

    Colocando verificação de ligado / desligado no toggleButton e switch

--- app/src/main/java/com/example/toggleswitch/MainActivity.java
+++ app/src/main/java/com/example/toggleswitch/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.toggleswitch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ToggleButton toggleSenha;
+    private Switch switchSenha;
+    private TextView textResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggleSenha =findViewById(R.id.toggleSenha);
+        switchSenha = findViewById(R.id.switchSenha);
+        textResultado = findViewById(R.id.textResultado);
+
     }
+    public void enviar(View view){
+
+        if(switchSenha.isChecked()){
+            textResultado.setText("switch ligado");
+        }else {
+            textResultado.setText("switch desligado");
+        }
+
+        if(toggleSenha.isChecked()){
+            textResultado.setText("toggle ligado");
+        }else {
+            textResultado.setText("toggle desligado");
+        }
+    }
+
 }
